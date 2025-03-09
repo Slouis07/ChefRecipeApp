e797907116e3316cb34ebb86eeeb711579009568
Slouis07 <142813774+Slouis07@users.noreply.github.com>
2025-03-09 07:47:24 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    dedupe: ['react', 'react-dom'],  // Deduplicate react and react-dom
-  },
   build: {
     rollupOptions: {
-    },
-    commonjsOptions: {
-      include: [/node_modules/],  // Include node_modules to handle mixed module types
-      transformMixedEsModules: true,  // Allow transformation of mixed modules
+      // external: ["@huggingface/inference", "react-markdown"],
     },
   },
 });
