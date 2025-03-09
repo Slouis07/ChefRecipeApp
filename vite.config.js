5f255ca0a0e9930da174ff81bb6464e57cabb217
Slouis07 <142813774+Slouis07@users.noreply.github.com>
2025-03-09 08:40:35 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
-  },
+  plugins: [react()],
   build: {
     rollupOptions: {
-       input: "/src/index.jsx",
-      // external: ["@huggingface/inference", "react-markdown"],
+      input: 'index.jsx',  
     },
   },
 });
